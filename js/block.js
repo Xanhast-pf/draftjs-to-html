46b047a17c073d896dd1edac31697db96dd4054e
Xanhast-pf <jbouquet47@gmail.com>
2017-08-29 12:44:46 -0400

    Loggar

--- js/block.js
+++ js/block.js
@@ -349,11 +349,10 @@ function getEntityMarkup(
         return `<a href="${entity.data.url}" target="${target}">${text}</a>`;
     }
     if (entity.type === "IMAGE") {
-        console.log("WUT entity: ", entity);
-        return `<img src="${entity.data.src}" alt="${entity.data
+        return `<div><img src="${entity.data.src}" alt="${entity.data
             .alt}" style="float:${entity.data.alignment ||
             "none"};" height="${entity.data.height}" width="${entity.data
-            .width}" />`;
+            .width}" /><p>${JSON.stringify(entity)}</p></div>`;
     }
     if (entity.type === "EMBEDDED_LINK") {
         return `<iframe width="${entity.data.width}" height="${entity.data
